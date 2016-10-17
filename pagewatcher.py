07aeedbf6ac7136bd7256269b23cc4dabe46005c
Mike Brown <mike.brown@excella.com>
2016-10-16 21:46:00 -0400

    Switch to beautifulsoup

--- pagewatcher.py
+++ pagewatcher.py
@@ -1,19 +1,23 @@
 import os
-from flask import Flask, send_file
-from selenium import webdriver  
+from flask import Flask
+from bs4 import BeautifulSoup
+import requests
+
 
 app = Flask(__name__)
 
-URL=http://barexam.virginia.gov/bar/barresults.html
-WAYBACK_URL=https://web.archive.org/web/20151101161538/http://barexam.virginia.gov/bar/barresults.html
+URL = 'http://barexam.virginia.gov/bar/barresults.html'
+WAYBACK_URL = 'https://web.archive.org/web/20151101161538/http://barexam.virginia.gov/bar/barresults.html'
 
 @app.route('/')
 def pagewatcher():
 
-    driver = webdriver.PhantomJS()
-    driver.set_window_size(1024, 768) 
-    driver.get(URL)
-    driver.save_screenshot('screen.png') 
-    driver.quit()
+    r = requests.get(WAYBACK_URL)
+    soup = BeautifulSoup(r.text)
 
-    return send_file('screen.png', mimetype='image/png')
+    mydivs = soup.findAll("div", { "class" : "panel-success" })
+
+    if len(mydivs) > 0:
+        return "GOT RESULTS!!"
+    else:
+        return "NO RESULTS :("
