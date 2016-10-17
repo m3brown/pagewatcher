703498db132ef56447f6d1fb44bf880a08ff7046
Mike Brown <mike.brown@excella.com>
2016-10-16 21:33:30 -0400

    Add pdfreactor

--- pagewatcher.py
+++ pagewatcher.py
@@ -1,8 +1,19 @@
 import os
-from flask import Flask
+from flask import Flask, send_file
+from selenium import webdriver  
 
 app = Flask(__name__)
 
+URL=http://barexam.virginia.gov/bar/barresults.html
+WAYBACK_URL=https://web.archive.org/web/20151101161538/http://barexam.virginia.gov/bar/barresults.html
+
 @app.route('/')
 def pagewatcher():
-    return 'Hello World!'
+
+    driver = webdriver.PhantomJS()
+    driver.set_window_size(1024, 768) 
+    driver.get(URL)
+    driver.save_screenshot('screen.png') 
+    driver.quit()
+
+    return send_file('screen.png', mimetype='image/png')
